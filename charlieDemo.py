3d931c7b24ca3e98841637e2aa6c9af7aec3b28d
Andy Harris <andy@aharrisbooks.net>
2024-10-31 10:13:50 -0400

    Add coin

--- charlieDemo.py
+++ charlieDemo.py
@@ -1,4 +1,4 @@
-import pygame, simpleGE
+import pygame, simpleGE, random
 
 """CharlieDemo
 """
@@ -16,14 +16,38 @@ class Charlie(simpleGE.Sprite):
             self.x -= self.moveSpeed
         if self.isKeyPressed(pygame.K_RIGHT):
             self.x += self.moveSpeed
+            
+class Coin(simpleGE.Sprite):
+    def __init__(self, scene):
+        super().__init__(scene)
+        self.setImage("Coin.png")
+        self.setSize(25, 25)
+        self.minSpeed = 3
+        self.maxSpeed = 8        
+        self.reset()
+
+    def reset(self):
+        self.x = random.randint(0, self.screenWidth)
+        self.y = 100
+        self.dy = random.randint(self.minSpeed, self.maxSpeed)
+        
+    def checkBounds(self):
+        if self.bottom > self.screenHeight:
+            self.reset()
 
 class Game(simpleGE.Scene):
     def __init__(self):
         super().__init__()
         self.setImage("campus.jpg")
         self.charlie = Charlie(self)
+        self.coin = Coin(self)
+ 
         
-        self.sprites = [self.charlie]
+        
+        self.sprites = [self.charlie,
+                        self.coin]
+    
+    
         
 def main():
     game = Game()
